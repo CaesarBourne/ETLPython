a9700eb33083fdd30ad6b4d3606fd1a0233c71b8
Emmanuel Adeleke <caesaradek@gmail.com>
2026-06-01 23:04:31 +0000

    Add read_transactions function to read and yield lines from a file

---
+++ csv.py
@@ -0,0 +1,11 @@
+def read_transactions(filename):
+
+    with open(filename, "r") as file:
+
+        for line in file:
+            yield line.strip()def read_transactions(filename):
+
+    with open(filename, "r") as file:
+
+        for line in file:
+            yield line.strip()
